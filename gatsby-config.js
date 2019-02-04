37a0525193173a7d5d51ea40d5a2270fddec2767
hembin <43884134+hembin@users.noreply.github.com>
2019-02-04 22:58:09 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
       options: {
         spaceId: `nkzud69evl1m`,
         // Learn about environment variables: https://gatsby.app/env-vars
-        accessToken: `c1e2f5bf828fc75143029297858698a52cac2d0a813ee833c62d6804a17d0bc5` process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken: `c1e2f5bf828fc75143029297858698a52cac2d0a813ee833c62d6804a17d0bc5`,
       },
     },
   ],
